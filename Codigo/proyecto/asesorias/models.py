bbe09122634aca377d69fd34d8bb88b3cd0fa2cc
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-03-30 01:43:04 +0000

    Creada la primera clase del modelo.

--- Codigo/proyecto/asesorias/models.py
+++ Codigo/proyecto/asesorias/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Centro(models.Model):
+	id_centro = models.AutoField(primary_key=True)
+	nombre_centro = models.CharField(max_length=25, unique=True)
+
+	def __unicode__(self):
+		return self.nombre_centro
