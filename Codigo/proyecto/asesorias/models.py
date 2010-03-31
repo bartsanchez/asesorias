af2df1ce239ee55d798696602ce39dc5ec20bc31
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-03-31 00:06:04 +0000

    Creada la clase del modelo AdministradorCentro.

--- Codigo/proyecto/asesorias/models.py
+++ Codigo/proyecto/asesorias/models.py
@@ -7,3 +7,10 @@ class Centro(models.Model):
 
 	def __unicode__(self):
 		return self.nombre_centro
+
+class AdministradorCentro(models.Model):
+	id_adm_centro = models.AutoField(primary_key=True)
+	nombre_adm_centro = models.CharField(max_length=25, unique=True)
+
+	def __unicode__(self):
+		return self.nombre_adm_centro
