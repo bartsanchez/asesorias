0d52fec69fce016655c8df51a7909a229e860c2d
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-04-11 00:09:55 +0000

    Establecido el campo como password.

--- Codigo/proyecto/asesorias/forms.py
+++ Codigo/proyecto/asesorias/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
 	nombre = forms.CharField(label='Nombre')
-	password = forms.CharField(label='Password')
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
