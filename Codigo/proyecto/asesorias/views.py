4ef8eff0348911a0379877fffeb7f244023b3f0c
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-04-12 00:40:07 +0000

    Mejorado el sistema de login.

--- Codigo/proyecto/asesorias/views.py
+++ Codigo/proyecto/asesorias/views.py
@@ -1,10 +1,40 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-from django.contrib.auth.forms import AuthenticationForm
+from asesorias.forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 def login(request):
+
+	def errorHandle(error):
+		form = LoginForm()
+		return render_to_response('asesorias/login.html', {
+				'error' : error,
+				'form' : form,
+		})
+
 	if request.method == 'POST':
-		form = AuthenticationForm(request.POST)
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			username = request.POST['username']
+			password = request.POST['password']
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					error = u'exito'
+					return errorHandle(error)
+				# Redirect to a success page.
+				else:
+					error = u'Cuenta desactivada.'
+					return errorHandle(error)
+				# Return a 'disabled account' error message
+			else:
+				error = u'Login no valido.'
+				return errorHandle(error)
+			# Return an 'invalid login' error message.
+		else:
+			error = u'Formulario erroneo'
+			return errorHandle(error)
 	else:
-		form = AuthenticationForm()
+		form = LoginForm()
 	return render_to_response('asesorias/login.html', {'form': form})
