19d6b47437a7c42ff3a821af469031ba4dc1cadb
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-04-12 00:58:20 +0000

    Corregido pequeño error.

--- Codigo/proyecto/asesorias/views.py
+++ Codigo/proyecto/asesorias/views.py
@@ -3,15 +3,8 @@ from django.shortcuts import render_to_response
 from asesorias.forms import LoginForm
 from django.contrib.auth import authenticate, login
 
-def login(request):
-
-	def errorHandle(error):
-		form = LoginForm()
-		return render_to_response('asesorias/login.html', {
-				'error' : error,
-				'form' : form,
-		})
-
+def authentication(request):
+	msg = []
 	if request.method == 'POST':
 		form = LoginForm(request.POST)
 		if form.is_valid():
@@ -21,20 +14,16 @@ def login(request):
 			if user is not None:
 				if user.is_active:
 					login(request, user)
-					error = u'exito'
-					return errorHandle(error)
+					msg.append('exito')
 				# Redirect to a success page.
 				else:
-					error = u'Cuenta desactivada.'
-					return errorHandle(error)
+					msg.append('Cuenta desactivada.')
 				# Return a 'disabled account' error message
 			else:
-				error = u'Login no valido.'
-				return errorHandle(error)
+				msg.append('Login no valido.')
 			# Return an 'invalid login' error message.
 		else:
-			error = u'Formulario erroneo'
-			return errorHandle(error)
+			msg.append('Formulario erroneo')
 	else:
 		form = LoginForm()
-	return render_to_response('asesorias/login.html', {'form': form})
+	return render_to_response('asesorias/autentificacion.html', {'form': form, 'error': msg})
