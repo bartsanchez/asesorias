f5971bb121909d43954f2377096c9a2c261e1684
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-08-05 15:31:31 +0000

    Sustituidos espacios por tabuladores en AdministradorPrincipal.

--- Codigo/proyecto/asesorias/vistas/vistasAdministradorPrincipal.py
+++ Codigo/proyecto/asesorias/vistas/vistasAdministradorPrincipal.py
@@ -1,26 +1,35 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 
+PATH = 'asesorias/AdministradorPrincipal/'
+
 @login_required()
 def administrador_inicio(request):
-	return render_to_response('asesorias/AdministradorPrincipal/administrador_inicio.html', {'user': request.user})
+    return render_to_response(PATH + 'administrador_inicio.html',
+        {'user': request.user})
 
 @login_required()
 def administrador_org_institucional(request):
-	return render_to_response('asesorias/AdministradorPrincipal/administrador_org_institucional.html', {'user': request.user})
+    return render_to_response(
+        PATH + 'administrador_org_institucional.html',
+        {'user': request.user})
 
 @login_required()
 def administrador_org_docente(request):
-	return render_to_response('asesorias/AdministradorPrincipal/administrador_org_docente.html', {'user': request.user})
+    return render_to_response(PATH + 'administrador_org_docente.html',
+        {'user': request.user})
 
 @login_required()
 def administrador_alumnos(request):
-	return render_to_response('asesorias/AdministradorPrincipal/administrador_alumnos.html', {'user': request.user})
+    return render_to_response(PATH + 'administrador_alumnos.html',
+        {'user': request.user})
 
 @login_required()
 def administrador_reuniones(request):
-	return render_to_response('asesorias/AdministradorPrincipal/administrador_reuniones.html', {'user': request.user})
+    return render_to_response(PATH + 'administrador_reuniones.html',
+        {'user': request.user})
 
 @login_required()
 def administrador_plantillas(request):
-	return render_to_response('asesorias/AdministradorPrincipal/administrador_plantillas.html', {'user': request.user})
+    return render_to_response(PATH + 'administrador_plantillas.html',
+        {'user': request.user})
