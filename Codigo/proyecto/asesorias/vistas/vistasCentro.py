27523b3ad4770d154be768e37d435497f7673fa0
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-08-05 14:37:14 +0000

    Corregido pequeño error.

--- Codigo/proyecto/asesorias/vistas/vistasCentro.py
+++ Codigo/proyecto/asesorias/vistas/vistasCentro.py
@@ -22,12 +22,14 @@ def addCentro(request):
 			# Se guarda la informacion del formulario en el sistema.
 			form.save()
 			# Redirige a la pagina de listar centros.
-			return HttpResponseRedirect( reverse('listCentro', kwargs={'orden': 'nombre_centro'}) )
+			return HttpResponseRedirect( reverse('listCentro',
+				kwargs={'orden': 'nombre_centro'}) )
 	# Si aun no se ha rellenado el formulario, se genera uno en blanco.
 	else:
 		form = forms.CentroForm()
 
-	return render_to_response('asesorias/Centro/addCentro.html', {'user': request.user, 'form': form})
+	return render_to_response('asesorias/Centro/addCentro.html',
+		{'user': request.user, 'form': form})
 
 def editCentro(request, centro):
 	# Se obtiene la instancia del centro.
@@ -44,11 +46,13 @@ def editCentro(request, centro):
 			if form.is_valid():
 				form.save()
 				# Redirige a la pagina de listar centros.
-				return HttpResponseRedirect( reverse('listCentro', kwargs={'orden': 'nombre_centro'}) )
+				return HttpResponseRedirect( reverse('listCentro',
+					kwargs={'orden': 'nombre_centro'}) )
 	# El centro no existe.
 	else:
 		form = False
-	return render_to_response('asesorias/Centro/editCentro.html', {'user': request.user, 'form': form})
+	return render_to_response('asesorias/Centro/editCentro.html',
+		{'user': request.user, 'form': form})
 
 def delCentro(request, centro):
 	# Se obtiene la instancia del centro.
@@ -57,11 +61,13 @@ def delCentro(request, centro):
 	if instancia_centro:
 		instancia_centro.delete()
 		# Redirige a la pagina de listar centros.
-		return HttpResponseRedirect( reverse('listCentro', kwargs={'orden': 'nombre_centro'}) )
+		return HttpResponseRedirect( reverse('listCentro',
+			kwargs={'orden': 'nombre_centro'}) )
 	# El centro no existe.
 	else:
 		error = True
-	return render_to_response('asesorias/Centro/delCentro.html', {'user': request.user, 'error': error})
+	return render_to_response('asesorias/Centro/delCentro.html',
+		{'user': request.user, 'error': error})
 
 def listCentro(request, orden):
 	# Se obtiene una lista con todos los centros.
@@ -74,7 +80,8 @@ def listCentro(request, orden):
 		# Si es valido se realiza la busqueda.
 		if form.is_valid():
 			busqueda = request.POST['busqueda']
-			lista_centros = lista_centros.filter(nombre_centro__contains=busqueda)
+			lista_centros = lista_centros.filter(
+				nombre_centro__contains=busqueda)
 		else:
 			busqueda = False
 	# No se ha realizado busqueda.
@@ -86,13 +93,17 @@ def listCentro(request, orden):
 		if orden == '_nombre_centro':
 			lista_centros = lista_centros.reverse()
 
-	return render_to_response('asesorias/Centro/listCentro.html', {'user': request.user, 'form': form, 'lista_centros': lista_centros, 'busqueda': busqueda, 'orden': orden})
+	return render_to_response('asesorias/Centro/listCentro.html',
+		{'user': request.user, 'form': form, 'lista_centros': lista_centros,
+		'busqueda': busqueda, 'orden': orden})
 
 def generarPDFListaCentros(request, busqueda):
 	lista_centros = models.Centro.objects.order_by('nombre_centro')
 
 	# Se ha realizado una busqueda.
 	if busqueda != 'False':
-		lista_centros = lista_centros.filter(nombre_centro__contains=busqueda)
+		lista_centros = lista_centros.filter(
+			nombre_centro__contains=busqueda)
 
-	return vistasPDF.render_to_pdf( 'asesorias/plantilla_pdf.html', {'mylist': lista_centros, 'name': 'centros',} )
+	return vistasPDF.render_to_pdf( 'asesorias/plantilla_pdf.html', 
+		{'mylist': lista_centros, 'name': 'centros',} )
