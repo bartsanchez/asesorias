054ae81f6b19dab95b2f2258fd0adec988376d66
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-08-11 16:08:25 +0000

    Sustituidos espacios por tabuladores en AlumnoCA.

--- Codigo/proyecto/asesorias/vistas/vistasAlumnoCursoAcademico.py
+++ Codigo/proyecto/asesorias/vistas/vistasAlumnoCursoAcademico.py
@@ -5,164 +5,216 @@ from asesorias import models, forms
 from asesorias.vistas import vistasAlumno
 from asesorias.utils import vistasPDF
 
-# Comprueba si existe un alumno curso academico y, de ser asi, la devuelve.
+PATH = 'asesorias/AlumnoCursoAcademico/'
+
+# Comprueba si existe un alumno curso academico y, de ser asi, la
+# devuelve.
 def obtenerAlumnoCursoAcademico(dni_pasaporte, curso_academico):
-	try:
-		# Obtiene la instancia de alumno curso academico.
-		resultado = models.AlumnoCursoAcademico.objects.get(dni_pasaporte=dni_pasaporte, curso_academico=curso_academico)
-	except:
-		resultado = False
-	return resultado
+    try:
+        # Obtiene la instancia de alumno curso academico.
+        resultado = models.AlumnoCursoAcademico.objects.get(
+            dni_pasaporte=dni_pasaporte,
+            curso_academico=curso_academico)
+    except:
+        resultado = False
+    return resultado
 
 def addAlumnoCursoAcademico(request):
-	# Se ha rellenado el formulario.
-	if request.method == 'POST':
-		# Se obtienen los valores y se valida.
-		form = forms.AlumnoCursoAcademicoForm(request.POST)
-		if form.is_valid():
-			# Se guarda la informacion del formulario en el sistema.
-			form.save()
+    # Se ha rellenado el formulario.
+    if request.method == 'POST':
+        # Se obtienen los valores y se valida.
+        form = forms.AlumnoCursoAcademicoForm(request.POST)
+        if form.is_valid():
+            # Se guarda la informacion del formulario en el sistema.
+            form.save()
 
-			dni_pasaporte = request.POST['dni_pasaporte']
+            dni_pasaporte = request.POST['dni_pasaporte']
 
-			# Redirige a la pagina de listar alumnos curso academico.
-			return HttpResponseRedirect( reverse('listAlumnoCursoAcademico', kwargs={'dni_pasaporte': dni_pasaporte, 'orden': 'curso_academico'}) )
-	# Si aun no se ha rellenado el formulario, se genera uno en blanco.
-	else:
-		form = forms.AlumnoCursoAcademicoForm()
-	return render_to_response('asesorias/AlumnoCursoAcademico/addAlumnoCursoAcademico.html', {'user': request.user, 'form': form})
+            # Redirige a la pagina de listar alumnos curso academico.
+            return HttpResponseRedirect(
+                reverse('listAlumnoCursoAcademico',
+                kwargs={'dni_pasaporte': dni_pasaporte,
+                'orden': 'curso_academico'}))
+    # Si aun no se ha rellenado el formulario, se genera uno en blanco.
+    else:
+        form = forms.AlumnoCursoAcademicoForm()
+    return render_to_response(PATH + 'addAlumnoCursoAcademico.html',
+        {'user': request.user, 'form': form})
 
 def editAlumnoCursoAcademico(request, dni_pasaporte, curso_academico):
-	# Se obtiene la instancia del alumno curso academico.
-	instancia_alumno_curso_academico= obtenerAlumnoCursoAcademico(dni_pasaporte, curso_academico)
-	# Si existe se edita.
-	if instancia_alumno_curso_academico:
-		# Se carga el formulario para la asignatura existente.
-		form = forms.AlumnoCursoAcademicoForm(instance=instancia_alumno_curso_academico)
-		# Se ha modificado el formulario original.
-		if request.method == 'POST':
-			# Se actualiza el formulario con la nueva informacion.
-			form = forms.AlumnoCursoAcademicoForm(request.POST, instance=instancia_alumno_curso_academico)
-			# Si es valido se guarda.
-			if form.is_valid():
-				form.save()
-				# Redirige a la pagina de listar alumnos curso academico.
-				return HttpResponseRedirect( reverse('listAlumnoCursoAcademico', kwargs={'dni_pasaporte': instancia_alumno_curso_academico.dni_pasaporte, 'orden': 'curso_academico'}) )
-	# El alumno curso academico no existe.
-	else:
-		form = False
-	return render_to_response('asesorias/AlumnoCursoAcademico/editAlumnoCursoAcademico.html', {'user': request.user, 'form': form})
+    # Se obtiene la instancia del alumno curso academico.
+    instancia_alumno_curso_academico= obtenerAlumnoCursoAcademico(
+        dni_pasaporte, curso_academico)
+    # Si existe se edita.
+    if instancia_alumno_curso_academico:
+        # Se carga el formulario para la asignatura existente.
+        form = forms.AlumnoCursoAcademicoForm(
+            instance=instancia_alumno_curso_academico)
+        # Se ha modificado el formulario original.
+        if request.method == 'POST':
+            # Se actualiza el formulario con la nueva informacion.
+            form = forms.AlumnoCursoAcademicoForm(request.POST,
+                instance=instancia_alumno_curso_academico)
+            # Si es valido se guarda.
+            if form.is_valid():
+                form.save()
+                # Redirige a la pagina de listar alumnos curso
+                # academico.
+                return HttpResponseRedirect(
+                    reverse('listAlumnoCursoAcademico',
+                    kwargs={'dni_pasaporte':
+                    instancia_alumno_curso_academico.dni_pasaporte,
+                    'orden': 'curso_academico'}))
+    # El alumno curso academico no existe.
+    else:
+        form = False
+    return render_to_response(PATH + 'editAlumnoCursoAcademico.html',
+        {'user': request.user, 'form': form})
 
 def delAlumnoCursoAcademico(request, dni_pasaporte, curso_academico):
-	# Se obtiene la instancia del alumno curso academico.
-	instancia_alumno_curso_academico= obtenerAlumnoCursoAcademico(dni_pasaporte, curso_academico)
-	# Si existe se elimina.
-	if instancia_alumno_curso_academico:
-		instancia_alumno_curso_academico.delete()
-		# Redirige a la pagina de listar alumnos curso academico.
-		return HttpResponseRedirect( reverse('listAlumnoCursoAcademico', kwargs={'dni_pasaporte': instancia_alumno_curso_academico.dni_pasaporte, 'orden': 'curso_academico'}) )
-	# El alumno curso academico no existe.
-	else:
-		error = True
-	return render_to_response('asesorias/AlumnoCursoAcademico/delAlumnoCursoAcademico.html', {'user': request.user, 'error': error})
+    # Se obtiene la instancia del alumno curso academico.
+    instancia_alumno_curso_academico= obtenerAlumnoCursoAcademico(
+        dni_pasaporte, curso_academico)
+    # Si existe se elimina.
+    if instancia_alumno_curso_academico:
+        instancia_alumno_curso_academico.delete()
+        # Redirige a la pagina de listar alumnos curso academico.
+        return HttpResponseRedirect(reverse('listAlumnoCursoAcademico',
+            kwargs={'dni_pasaporte':
+            instancia_alumno_curso_academico.dni_pasaporte,
+            'orden': 'curso_academico'}))
+    # El alumno curso academico no existe.
+    else:
+        error = True
+    return render_to_response(PATH + 'delAlumnoCursoAcademico.html',
+        {'user': request.user, 'error': error})
 
 def selectAlumno(request):
-	# Se ha introducido un alumno.
-	if request.method == 'POST':
+    # Se ha introducido un alumno.
+    if request.method == 'POST':
 
-		# Se obtiene el alumno y se valida.
-		form = forms.AlumnoFormSelect(request.POST)
+        # Se obtiene el alumno y se valida.
+        form = forms.AlumnoFormSelect(request.POST)
 
-		# Si es valido se redirige a listar alumnos curso academico.
-		if form.is_valid():
-			alumno = request.POST['alumno']
+        # Si es valido se redirige a listar alumnos curso academico.
+        if form.is_valid():
+            alumno = request.POST['alumno']
 
-			# Se crea una instancia del alumno para pasar el nombre de alumno por argumento.
-			instancia_alumno = models.Alumno.objects.get(pk=alumno)
+            # Se crea una instancia del alumno para pasar el nombre de
+            # alumno por argumento.
+            instancia_alumno = models.Alumno.objects.get(pk=alumno)
 
-			return HttpResponseRedirect( reverse('listAlumnoCursoAcademico', kwargs={'dni_pasaporte': alumno, 'orden': 'curso_academico'}) )
+            return HttpResponseRedirect(
+                reverse('listAlumnoCursoAcademico',
+                kwargs={'dni_pasaporte': alumno,
+                'orden': 'curso_academico'}))
 
-		else:
-			HttpResponseRedirect( reverse('selectAlumno_AlumnoCursoAcademico') )
+        else:
+            HttpResponseRedirect(
+                reverse('selectAlumno_AlumnoCursoAcademico'))
 
-	else:
-		form = forms.AlumnoFormSelect()
+    else:
+        form = forms.AlumnoFormSelect()
 
-	return render_to_response('asesorias/AlumnoCursoAcademico/selectAlumno.html', {'user': request.user, 'form': form})
+    return render_to_response(PATH + 'selectAlumno.html',
+        {'user': request.user, 'form': form})
 
 def listAlumnoCursoAcademico(request, dni_pasaporte, orden):
-	# Se comprueba que exista el alumno pasado por argumento.
-	instancia_alumno = vistasAlumno.obtenerAlumno(dni_pasaporte)
+    # Se comprueba que exista el alumno pasado por argumento.
+    instancia_alumno = vistasAlumno.obtenerAlumno(dni_pasaporte)
 
-	# El alumno no existe, se redirige.
-	if not (instancia_alumno):
-		return HttpResponseRedirect( reverse('selectAlumno_AlumnoCursoAcademico') )
+    # El alumno no existe, se redirige.
+    if not (instancia_alumno):
+        return HttpResponseRedirect(
+            reverse('selectAlumno_AlumnoCursoAcademico'))
 
-	# Se obtiene una lista con todos los alumnos curso academico.
-	lista_alumnos_curso_academico = models.AlumnoCursoAcademico.objects.filter(dni_pasaporte=dni_pasaporte).order_by('curso_academico')
+    # Se obtiene una lista con todos los alumnos curso academico.
+    lista_alumnos_curso_academico = \
+        models.AlumnoCursoAcademico.objects.filter(
+        dni_pasaporte=dni_pasaporte).order_by('curso_academico')
 
-	# Se ha realizado una busqueda.
-	if request.method == 'POST':
-		# Se obtienen los valores y se valida.
-		form = forms.SearchForm(request.POST)
-		# Si es valido se realiza la busqueda.
-		if form.is_valid():
-			busqueda = request.POST['busqueda']
+    # Se ha realizado una busqueda.
+    if request.method == 'POST':
+        # Se obtienen los valores y se valida.
+        form = forms.SearchForm(request.POST)
+        # Si es valido se realiza la busqueda.
+        if form.is_valid():
+            busqueda = request.POST['busqueda']
 
-			# Se crea una lista auxiliar que albergara el resultado de la busqueda.
-			lista_aux = []
+            # Se crea una lista auxiliar que albergara el resultado de
+            # la busqueda.
+            lista_aux = []
 
-			# Se recorren los elementos determinando si coinciden con la busqueda.
-			for alumno in lista_alumnos_curso_academico:
-				# Se crea una cadena auxiliar para examinar si se encuentra el resultado de la busqueda.
-				cadena = unicode(alumno.curso_academico)
+            # Se recorren los elementos determinando si coinciden con
+            # la busqueda.
+            for alumno in lista_alumnos_curso_academico:
+                # Se crea una cadena auxiliar para examinar si se
+                # encuentra el resultado de la busqueda.
+                cadena = unicode(alumno.curso_academico)
 
-				# Si se encuentra la busqueda el elemento se incluye en la lista auxiliar.
-				if cadena.find(busqueda) >= 0:
-					lista_aux.append(alumno)
+                # Si se encuentra la busqueda el elemento se incluye en
+                # la lista auxiliar.
+                if cadena.find(busqueda) >= 0:
+                    lista_aux.append(alumno)
 
-			# La lista final a devolver sera la lista auxiliar.
-			lista_alumnos_curso_academico = lista_aux
+            # La lista final a devolver sera la lista auxiliar.
+            lista_alumnos_curso_academico = lista_aux
 
-		else:
-			busqueda = False
-	# No se ha realizado busqueda.
-	else:
-		# Formulario para una posible busqueda.
-		form = forms.SearchForm()
-		busqueda = False
+        else:
+            busqueda = False
+    # No se ha realizado busqueda.
+    else:
+        # Formulario para una posible busqueda.
+        form = forms.SearchForm()
+        busqueda = False
 
-		if (orden == '_curso_academico'):
-			lista_alumnos_curso_academico = reversed(lista_alumnos_curso_academico)
+        if (orden == '_curso_academico'):
+            lista_alumnos_curso_academico = \
+                reversed(lista_alumnos_curso_academico)
 
-	return render_to_response('asesorias/AlumnoCursoAcademico/listAlumnoCursoAcademico.html', {'user': request.user, 'form': form, 'lista_alumnos_curso_academico': lista_alumnos_curso_academico, 'busqueda': busqueda, 'alumno': dni_pasaporte, 'orden': orden})
+    return render_to_response(PATH + 'listAlumnoCursoAcademico.html',
+        {'user': request.user, 'form': form,
+        'lista_alumnos_curso_academico': lista_alumnos_curso_academico,
+        'busqueda': busqueda, 'alumno': dni_pasaporte, 'orden': orden})
 
-def generarPDFListaAlumnosCursoAcademico(request, dni_pasaporte, busqueda):
-	# Se comprueba que exista el alumno pasado por argumento.
-	existe_alumno_curso_academico = models.AlumnoCursoAcademico.objects.filter(dni_pasaporte=dni_pasaporte)
+def generarPDFListaAlumnosCursoAcademico(request, dni_pasaporte,
+    busqueda):
+    # Se comprueba que exista el alumno pasado por argumento.
+    existe_alumno_curso_academico = \
+        models.AlumnoCursoAcademico.objects.filter(
+        dni_pasaporte=dni_pasaporte)
 
-	# El alumno no existe, se redirige.
-	if not (existe_alumno_curso_academico):
-		return HttpResponseRedirect( reverse('selectAlumno_AlumnoCursoAcademico') )
+    # El alumno no existe, se redirige.
+    if not (existe_alumno_curso_academico):
+        return HttpResponseRedirect(
+            reverse('selectAlumno_AlumnoCursoAcademico'))
 
-	# Se obtiene una lista con todos los alumnos curso academico.
-	lista_alumnos_curso_academico = models.AlumnoCursoAcademico.objects.filter(dni_pasaporte=dni_pasaporte).order_by('dni_pasaporte')
+    # Se obtiene una lista con todos los alumnos curso academico.
+    lista_alumnos_curso_academico = \
+        models.AlumnoCursoAcademico.objects.filter(
+        dni_pasaporte=dni_pasaporte).order_by('dni_pasaporte')
 
-	# Se ha realizado una busqueda.
-	if busqueda != 'False':
-		# Se crea una lista auxiliar que albergara el resultado de la busqueda.
-		lista_aux = []
+    # Se ha realizado una busqueda.
+    if busqueda != 'False':
+        # Se crea una lista auxiliar que albergara el resultado de la
+        # busqueda.
+        lista_aux = []
 
-		# Se recorren los elementos determinando si coinciden con la busqueda.
-		for alumno in lista_alumnos_curso_academico:
-			# Se crea una cadena auxiliar para examinar si se encuentra el resultado de la busqueda.
-			cadena = unicode(alumno.curso_academico)
+        # Se recorren los elementos determinando si coinciden con la
+        # busqueda.
+        for alumno in lista_alumnos_curso_academico:
+            # Se crea una cadena auxiliar para examinar si se encuentra
+            # el resultado de la busqueda.
+            cadena = unicode(alumno.curso_academico)
 
-			# Si se encuentra la busqueda el elemento se incluye en la lista auxiliar.
-			if cadena.find(busqueda) >= 0:
-				lista_aux.append(alumno)
+            # Si se encuentra la busqueda el elemento se incluye en la
+            # lista auxiliar.
+            if cadena.find(busqueda) >= 0:
+                lista_aux.append(alumno)
 
-		# La lista final a devolver sera la lista auxiliar.
-		lista_alumnos_curso_academico = lista_aux
+        # La lista final a devolver sera la lista auxiliar.
+        lista_alumnos_curso_academico = lista_aux
 
-	return vistasPDF.render_to_pdf( 'asesorias/plantilla_pdf.html', {'mylist': lista_alumnos_curso_academico, 'name': 'alumnos curso academico',} )
+    return vistasPDF.render_to_pdf('asesorias/plantilla_pdf.html',
+        {'mylist': lista_alumnos_curso_academico,
+        'name': 'alumnos curso academico',})
