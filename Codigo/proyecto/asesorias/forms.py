8d2c8366582945dce1390ec36dfed138364e6b8a
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-08-18 16:43:26 +0000

    Sustituidos espacios por tabuladores en forms.py.

--- Codigo/proyecto/asesorias/forms.py
+++ Codigo/proyecto/asesorias/forms.py
@@ -3,14 +3,16 @@ from asesorias import models
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Nombre", max_length=50)
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password = forms.CharField(label="Password",
+        widget=forms.PasswordInput)
 
 class CentroForm(forms.ModelForm):
     class Meta:
         model = models.Centro
 
 class CentroFormSelect(forms.Form):
-    centro = forms.ModelChoiceField(models.Centro.objects.order_by('nombre_centro'))
+    centro = forms.ModelChoiceField(models.Centro.objects.order_by(
+        'nombre_centro'))
 
 class AdministradorCentroForm(forms.ModelForm):
     class Meta:
@@ -21,63 +23,84 @@ class TitulacionForm(forms.ModelForm):
         model = models.Titulacion
 
 class TitulacionFormSelect(forms.Form):
-    titulacion = forms.ModelChoiceField(queryset=models.Titulacion.objects.all())
+    titulacion = forms.ModelChoiceField(
+        queryset=models.Titulacion.objects.all())
 
-    # Necesario para actualizar el queryset en tiempo de ejecucion, a traves del argumento id_centro.
+    # Necesario para actualizar el queryset en tiempo de ejecucion, a
+    # traves del argumento id_centro.
     def __init__(self, id_centro, *args, **kwargs):
         super(TitulacionFormSelect, self).__init__(*args, **kwargs)
-        self.fields['titulacion'].queryset = models.Titulacion.objects.filter(id_centro=id_centro).order_by('nombre_titulacion')
+        self.fields['titulacion'].queryset = \
+            models.Titulacion.objects.filter(
+            id_centro=id_centro).order_by('nombre_titulacion')
 
 class AsignaturaForm(forms.ModelForm):
     class Meta:
         model = models.Asignatura
 
 class AsignaturaFormSelect(forms.Form):
-    asignatura = forms.ModelChoiceField(queryset=models.Asignatura.objects.all())
+    asignatura = forms.ModelChoiceField(
+        queryset=models.Asignatura.objects.all())
 
-    # Necesario para actualizar el queryset en tiempo de ejecucion, a traves de los argumentos id_centro e id_titulacion.
+    # Necesario para actualizar el queryset en tiempo de ejecucion, a
+    # traves de los argumentos id_centro e id_titulacion.
     def __init__(self, id_centro, id_titulacion, *args, **kwargs):
         super(AsignaturaFormSelect, self).__init__(*args, **kwargs)
-        self.fields['asignatura'].queryset = models.Asignatura.objects.filter(id_centro=id_centro, id_titulacion=id_titulacion).order_by('nombre_asignatura')
+        self.fields['asignatura'].queryset = \
+            models.Asignatura.objects.filter(
+            id_centro=id_centro, id_titulacion=id_titulacion).order_by(
+            'nombre_asignatura')
 
 class AsignaturaCursoAcademicoForm(forms.ModelForm):
     class Meta:
         model = models.AsignaturaCursoAcademico
 
 class AsignaturaCursoAcademicoFormSelect(forms.Form):
-    asignatura_curso_academico = forms.ModelChoiceField(queryset=models.AsignaturaCursoAcademico.objects.all())
+    asignatura_curso_academico = forms.ModelChoiceField(
+        queryset=models.AsignaturaCursoAcademico.objects.all())
 
-    # Necesario para actualizar el queryset en tiempo de ejecucion, a traves de los argumentos id_centro e id_titulacion e id_asignatura.
-    def __init__(self, id_centro, id_titulacion, id_asignatura, *args, **kwargs):
-        super(AsignaturaCursoAcademicoFormSelect, self).__init__(*args, **kwargs)
-        self.fields['asignatura_curso_academico'].queryset = models.AsignaturaCursoAcademico.objects.filter(id_centro=id_centro, id_titulacion=id_titulacion, id_asignatura=id_asignatura).order_by('curso_academico')
+    # Necesario para actualizar el queryset en tiempo de ejecucion, a
+    # traves de los argumentos id_centro e id_titulacion e
+    # id_asignatura.
+    def __init__(self, id_centro, id_titulacion, id_asignatura,
+        *args, **kwargs):
+        super(AsignaturaCursoAcademicoFormSelect,
+            self).__init__(*args, **kwargs)
+        self.fields['asignatura_curso_academico'].queryset = \
+            models.AsignaturaCursoAcademico.objects.filter(
+            id_centro=id_centro, id_titulacion=id_titulacion,
+            id_asignatura=id_asignatura).order_by('curso_academico')
 
 class DepartamentoForm(forms.ModelForm):
     class Meta:
         model = models.Departamento
 
 class DepartamentoFormSelect(forms.Form):
-    departamento = forms.ModelChoiceField(models.Departamento.objects.order_by('nombre_departamento'))
+    departamento = forms.ModelChoiceField(
+        models.Departamento.objects.order_by('nombre_departamento'))
 
 class AsesorForm(forms.ModelForm):
     class Meta:
         model = models.Asesor
 
 class AsesorFormSelect(forms.Form):
-    asesor = forms.ModelChoiceField(models.Asesor.objects.order_by('dni_pasaporte'))
+    asesor = forms.ModelChoiceField(models.Asesor.objects.order_by(
+        'dni_pasaporte'))
 
 class AsesorCursoAcademicoForm(forms.ModelForm):
     class Meta:
         model = models.AsesorCursoAcademico
 
 class PlantillaEntrevistaAsesorForm(forms.ModelForm):
-    asesor_curso_academico = forms.ModelChoiceField(models.AsesorCursoAcademico.objects.all())
+    asesor_curso_academico = forms.ModelChoiceField(
+        models.AsesorCursoAcademico.objects.all())
 
     class Meta:
         model = models.PlantillaEntrevistaAsesor
 
 class PreguntaAsesorForm(forms.ModelForm):
-    plantilla_entrevista_asesor = forms.ModelChoiceField(models.PlantillaEntrevistaAsesor.objects.all())
+    plantilla_entrevista_asesor = forms.ModelChoiceField(
+        models.PlantillaEntrevistaAsesor.objects.all())
 
     class Meta:
         model = models.PreguntaAsesor
@@ -87,19 +110,25 @@ class AlumnoForm(forms.ModelForm):
         model = models.Alumno
 
 class AlumnoFormSelect(forms.Form):
-    alumno = forms.ModelChoiceField(models.Alumno.objects.order_by('dni_pasaporte'))
+    alumno = forms.ModelChoiceField(models.Alumno.objects.order_by(
+        'dni_pasaporte'))
 
 class AlumnoCursoAcademicoForm(forms.ModelForm):
     class Meta:
         model = models.AlumnoCursoAcademico
 
 class AlumnoCursoAcademicoFormSelect(forms.Form):
-    alumno = forms.ModelChoiceField(queryset=models.AlumnoCursoAcademico.objects.all())
+    alumno = forms.ModelChoiceField(
+        queryset=models.AlumnoCursoAcademico.objects.all())
 
-    # Necesario para actualizar el queryset en tiempo de ejecucion, a traves del argumento curso_academico.
+    # Necesario para actualizar el queryset en tiempo de ejecucion, a
+    # traves del argumento curso_academico.
     def __init__(self, curso_academico, *args, **kwargs):
-        super(AlumnoCursoAcademicoFormSelect, self).__init__(*args, **kwargs)
-        self.fields['alumno'].queryset = models.AlumnoCursoAcademico.objects.filter(curso_academico=curso_academico).order_by('dni_pasaporte')
+        super(AlumnoCursoAcademicoFormSelect,
+            self).__init__(*args, **kwargs)
+        self.fields['alumno'].queryset = \
+            models.AlumnoCursoAcademico.objects.filter(
+            curso_academico=curso_academico).order_by('dni_pasaporte')
 
 class MatriculaForm(forms.ModelForm):
     class Meta:
@@ -116,13 +145,15 @@ class PlantillaEntrevistaOficialForm(forms.ModelForm):
         model = models.PlantillaEntrevistaOficial
 
 class PreguntaOficialForm(forms.ModelForm):
-    plantilla_entrevista_oficial = forms.ModelChoiceField(models.PlantillaEntrevistaOficial.objects.all())
+    plantilla_entrevista_oficial = forms.ModelChoiceField(
+        models.PlantillaEntrevistaOficial.objects.all())
 
     class Meta:
         model = models.PreguntaOficial
 
 class ReunionForm(forms.ModelForm):
-    alumno_curso_academico = forms.ModelChoiceField(models.AlumnoCursoAcademico.objects.all())
+    alumno_curso_academico = forms.ModelChoiceField(
+        models.AlumnoCursoAcademico.objects.all())
 
     class Meta:
         model = models.Reunion
@@ -133,7 +164,8 @@ class Centro_AdministradorCentroForm(forms.ModelForm):
 
 class Reunion_PreguntaAsesorForm(forms.ModelForm):
     reunion = forms.ModelChoiceField(models.Reunion.objects.all())
-    pregunta_asesor = forms.ModelChoiceField(models.PreguntaAsesor.objects.all())
+    pregunta_asesor = forms.ModelChoiceField(
+        models.PreguntaAsesor.objects.all())
 
     class Meta:
         model = models.ReunionPreguntaAsesor
