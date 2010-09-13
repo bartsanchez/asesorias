e03fef3c4281418285373cc32036638a7ab20270
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-09-13 02:19:27 +0000

    Modificado el login.

--- Codigo/proyecto/asesorias/forms.py
+++ Codigo/proyecto/asesorias/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from asesorias import models
 
+CHOICES = [('administradorPrincipal', 'Administrador principal'),
+    ('administradorCentro','Administrador de centro'), ('asesor','Asesor'), ('alumno','Alumno')]
+
 class LoginForm(forms.Form):
     username = forms.CharField(label="Nombre", max_length=50)
     password = forms.CharField(label="Password",
         widget=forms.PasswordInput)
+    rol = forms.ChoiceField(choices=CHOICES)
 
 class CentroForm(forms.ModelForm):
     class Meta:
--- Codigo/proyecto/asesorias/vistas/vistasGestionUsuarios.py
+++ Codigo/proyecto/asesorias/vistas/vistasGestionUsuarios.py
@@ -7,62 +7,65 @@ from asesorias.vistas import vistasAdministradorCentro, vistasAsesor, vistasAlum
 
 # Vista que controla la autentificacion en el sistema.
 def authentication(request):
-	# Se ha rellenado el formulario.
-	if request.method == 'POST':
-		# Se obtienen los valores y se valida.
-		form = forms.LoginForm(request.POST)
-		if form.is_valid():
-			# Se autentifica el usuario.
-			username = request.POST['username']
-			password = request.POST['password']
-			user = authenticate(username=username, password=password)
-			# Si existe el usuario.
-			if user is not None:
-				if user.is_active:
-					login(request, user)
+    error = ''
+    # Se ha rellenado el formulario.
+    if request.method == 'POST':
+        # Se obtienen los valores y se valida.
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            # Se autentifica el usuario.
+            username = request.POST['username']
+            password = request.POST['password']
+            rol = request.POST['rol']
+            user = authenticate(username=username, password=password)
+            # Si existe el usuario.
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
 
-					# Comprueba si el usuario es administrador principal.
-					if user.is_superuser:
-						return HttpResponseRedirect( reverse('administrador_inicio') )
+                    if rol == 'administradorPrincipal':
+                        # Comprueba si el usuario es administrador
+                        # principal.
+                        if user.is_superuser:
+                            return HttpResponseRedirect(
+                                reverse('administrador_inicio'))
 
-					# Obtiene el rol del usuario (Administrador centro, asesor o alumno).
-					rol = obtenerRol(username)
+                    elif rol == 'administradorCentro':
+                        print 'hola'
+                    elif rol == 'asesor':
+                        return HttpResponseRedirect(
+                            reverse('vista_asesor'))
+                    elif rol == 'alumno':
+                        return HttpResponseRedirect(
+                            reverse('vista_alumno'))
 
-					# Si es asesor se redirige a la pagina de asesores.
-					if rol == 'asesor':
-						return HttpResponseRedirect( reverse('vista_asesor') )
-					# Si es alumno se redirige a la pagina de alumnos.
-					elif rol == 'alumno':
-						return HttpResponseRedirect( reverse('vista_alumno') )
-					else:
-						error = 'El usuario no tiene rol.'
-				else:
-					error = 'Cuenta desactivada.'
-			else:
-				error = 'Login no valido.'
-		else:
-			error = 'Formulario erroneo.'
-	# Si aun no se ha rellenado el formulario, se genera uno en blanco.
-	else:
-		form = forms.LoginForm()
-		error = ''
-	return render_to_response('asesorias/Login/login.html', {'form': form, 'error': error})
+                else:
+                    error = 'Cuenta desactivada.'
+            else:
+                error = 'Login no valido.'
+        else:
+            error = 'Formulario erroneo.'
+    # Si aun no se ha rellenado el formulario, se genera uno en blanco.
+    else:
+        form = forms.LoginForm()
+        error = ''
+    return render_to_response('asesorias/Login/login.html', {'form': form, 'error': error})
 
 # Vista que controla la salida del sistema de un usuario.
 def logout_view(request):
-	logout(request)
+    logout(request)
 
-	# Redirige a la pagina inicial.
-	return HttpResponseRedirect( reverse('authentication') )
+    # Redirige a la pagina inicial.
+    return HttpResponseRedirect( reverse('authentication') )
 
 # Determina el con el que participa un usuario de la aplicacion.
 def obtenerRol(username):
-	if vistasAdministradorCentro.obtenerAdministradorCentro(username):
-		rol = 'administradorCentro'
-	elif vistasAsesor.obtenerAsesor(username):
-		rol = 'asesor'
-	elif vistasAlumno.obtenerAlumno(username):
-		rol = 'alumno'
-	else:
-		rol = 'inactivo'
-	return rol
+    if vistasAdministradorCentro.obtenerAdministradorCentro(username):
+        rol = 'administradorCentro'
+    elif vistasAsesor.obtenerAsesor(username):
+        rol = 'asesor'
+    elif vistasAlumno.obtenerAlumno(username):
+        rol = 'alumno'
+    else:
+        rol = 'inactivo'
+    return rol
