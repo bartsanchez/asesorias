4de6bc168147ec6ba7c6f1f51772e80a25e047c1
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-09-15 15:43:11 +0000

    Editatas las url's.

--- Codigo/proyecto/asesorias/urls/administradorPrincipal.py
+++ Codigo/proyecto/asesorias/urls/administradorPrincipal.py
@@ -1,12 +1,16 @@
 from django.conf.urls.defaults import *
 
 VISTAS = 'asesorias.vistas.'
+URLS = 'asesorias.urls.'
 
 # -------------------------------- #
 # Url's de administrador principal #
 # -------------------------------- #
 
 urlpatterns = patterns(VISTAS + 'vistasAdministradorPrincipal',
+
+    ### URLS del menu horizontal. ###
+
     url(r'^$', 'administrador_inicio', name='administrador_inicio'),
 
     url(r'^organizacion_institucional/$',
@@ -24,4 +28,63 @@ urlpatterns = patterns(VISTAS + 'vistasAdministradorPrincipal',
 
     url(r'^plantillas/$',
         'administrador_plantillas', name='administrador_plantillas'),
+
+    ### Includes a cada entidad ###
+
+    (r'^centro/',
+        include(URLS + 'centro')),
+
+    (r'^administradorCentro/',
+        include(URLS + 'administradorCentro')),
+
+    (r'^titulacion/',
+        include(URLS + 'titulacion')),
+
+    (r'^asignatura/',
+        include(URLS + 'asignatura')),
+
+    (r'^asignaturaCursoAcademico/',
+        include(URLS + 'asignaturaCursoAcademico')),
+
+    (r'^departamento/',
+        include(URLS + 'departamento')),
+
+    (r'^asesor/',
+        include(URLS + 'asesor')),
+
+    (r'^asesorCursoAcademico/',
+        include(URLS + 'asesorCursoAcademico')),
+
+    (r'^plantillaEntrevistaAsesor/',
+        include(URLS + 'plantillaEntrevistaAsesor')),
+
+    (r'^preguntaAsesor/',
+        include(URLS + 'preguntaAsesor')),
+
+    (r'^alumno/',
+        include(URLS + 'alumno')),
+
+    (r'^alumnoCursoAcademico/',
+        include(URLS + 'alumnoCursoAcademico')),
+
+    (r'^matricula/',
+        include(URLS + 'matricula')),
+
+    (r'^calificacionConvocatoria/',
+        include(URLS + 'calificacionConvocatoria')),
+
+    (r'^plantillaEntrevistaOficial/',
+        include(URLS + 'plantillaEntrevistaOficial')),
+
+    (r'^preguntaOficial/',
+        include(URLS + 'preguntaOficial')),
+
+    (r'^reunion/',
+        include(URLS + 'reunion')),
+
+    (r'^centro_administradorCentro/',
+        include(URLS + 'centro_administradorCentro')),
+
+    (r'^reunion_preguntaAsesor/',
+        include(URLS + 'reunion_preguntaAsesor')),
 )
--- Codigo/proyecto/urls.py
+++ Codigo/proyecto/urls.py
@@ -23,63 +23,6 @@ urlpatterns = patterns('',
 
     (r'^asesorias/administradorPrincipal/',
         include(URLS + 'administradorPrincipal')),
-
-    (r'^asesorias/administradorPrincipal/centro/',
-        include(URLS + 'centro')),
-
-    (r'^asesorias/administradorPrincipal/administradorCentro/',
-        include(URLS + 'administradorCentro')),
-
-    (r'^asesorias/administradorPrincipal/titulacion/',
-        include(URLS + 'titulacion')),
-
-    (r'^asesorias/administradorPrincipal/asignatura/',
-        include(URLS + 'asignatura')),
-
-    (r'^asesorias/administradorPrincipal/asignaturaCursoAcademico/',
-        include(URLS + 'asignaturaCursoAcademico')),
-
-    (r'^asesorias/administradorPrincipal/departamento/',
-        include(URLS + 'departamento')),
-
-    (r'^asesorias/administradorPrincipal/asesor/',
-        include(URLS + 'asesor')),
-
-    (r'^asesorias/administradorPrincipal/asesorCursoAcademico/',
-        include(URLS + 'asesorCursoAcademico')),
-
-    (r'^asesorias/administradorPrincipal/plantillaEntrevistaAsesor/',
-        include(URLS + 'plantillaEntrevistaAsesor')),
-
-    (r'^asesorias/administradorPrincipal/preguntaAsesor/',
-        include(URLS + 'preguntaAsesor')),
-
-    (r'^asesorias/administradorPrincipal/alumno/',
-        include(URLS + 'alumno')),
-
-    (r'^asesorias/administradorPrincipal/alumnoCursoAcademico/',
-        include(URLS + 'alumnoCursoAcademico')),
-
-    (r'^asesorias/administradorPrincipal/matricula/',
-        include(URLS + 'matricula')),
-
-    (r'^asesorias/administradorPrincipal/calificacionConvocatoria/',
-        include(URLS + 'calificacionConvocatoria')),
-
-    (r'^asesorias/administradorPrincipal/plantillaEntrevistaOficial/',
-        include(URLS + 'plantillaEntrevistaOficial')),
-
-    (r'^asesorias/administradorPrincipal/preguntaOficial/',
-        include(URLS + 'preguntaOficial')),
-
-    (r'^asesorias/administradorPrincipal/reunion/',
-        include(URLS + 'reunion')),
-
-    (r'^asesorias/administradorPrincipal/centro_administradorCentro/',
-        include(URLS + 'centro_administradorCentro')),
-
-    (r'^asesorias/administradorPrincipal/reunion_preguntaAsesor/',
-        include(URLS + 'reunion_preguntaAsesor')),
 )
 
 # ------------------------- #
