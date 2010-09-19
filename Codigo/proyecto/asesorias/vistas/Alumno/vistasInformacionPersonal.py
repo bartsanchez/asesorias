959b04ca71a06efb79edc3a1179fc6d90d91699b
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-09-19 23:50:32 +0000

    Creada la vista showInfo_Alumno.

--- Codigo/proyecto/asesorias/vistas/Alumno/vistasInformacionPersonal.py
+++ Codigo/proyecto/asesorias/vistas/Alumno/vistasInformacionPersonal.py
@@ -23,14 +23,36 @@ def showInfo(request):
             correo_electronico = request.POST['correo_electronico']
             nombre = request.POST['nombre']
             apellidos = request.POST['apellidos']
-            telefono = request.POST['telefono']
+            fecha_nacimiento = request.POST['fecha_nacimiento']
+            direccion_cordoba = request.POST['direccion_cordoba']
+            localidad_familiar = request.POST['localidad_familiar']
+            provincia_familiar = request.POST['provincia_familiar']
+            codigo_postal = request.POST['codigo_postal']
+            telefono_familiar = request.POST['telefono_familiar']
+            ingreso = request.POST['ingreso']
+            otros_estudios_universitarios = request.POST[
+                'otros_estudios_universitarios']
+            modalidad_acceso_universidad = request.POST[
+                'modalidad_acceso_universidad']
+            calificacion_acceso = request.POST['calificacion_acceso']
 
             # Datos necesarios para actualizar el alumno.
             datos_alumno = {'dni_pasaporte': dni_pasaporte,
                 'correo_electronico': correo_electronico,
                 'nombre': nombre,
                 'apellidos': apellidos,
-                'telefono': telefono}
+                'fecha_nacimiento': fecha_nacimiento,
+                'direccion_cordoba': direccion_cordoba,
+                'localidad_familiar': localidad_familiar,
+                'provincia_familiar': provincia_familiar,
+                'codigo_postal': codigo_postal,
+                'telefono_familiar': telefono_familiar,
+                'ingreso': ingreso,
+                'otros_estudios_universitarios':
+                otros_estudios_universitarios,
+                'modalidad_acceso_universidad':
+                modalidad_acceso_universidad,
+                'calificacion_acceso': calificacion_acceso}
 
             # Se actualiza el formulario con la nueva informacion.
             form = forms.AlumnoForm(datos_alumno,
