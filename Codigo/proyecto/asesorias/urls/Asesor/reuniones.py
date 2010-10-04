e83d25cfee57f539a6e2a9ca573dd2c359d6cb16
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-10-04 10:20:21 +0000

    Corregido pequeño error.

---
+++ Codigo/proyecto/asesorias/urls/Asesor/reuniones.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+
+VISTAS = 'asesorias.vistas.Asesor.'
+
+# ------------------------#
+# Url's de usuario asesor #
+# ------------------------#
+
+urlpatterns = patterns(VISTAS + 'vistasReuniones',
+
+)
---
+++ Codigo/proyecto/asesorias/vistas/Asesor/vistasReuniones.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from asesorias import models, forms
+
+PATH = 'asesorias/UsuarioAsesor/'
+
+
