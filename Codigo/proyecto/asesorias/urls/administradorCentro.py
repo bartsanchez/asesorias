5c4523dbbadf14bbd76dfd78c45520cd7fa96421
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-10-05 22:18:48 +0000

    Creada la estructura necesaria para administrador de centro.

--- Codigo/proyecto/asesorias/urls/administradorCentro.py
+++ Codigo/proyecto/asesorias/urls/administradorCentro.py
@@ -14,6 +14,26 @@ urlpatterns = patterns(VISTAS + 'vistasAdministradorCentro',
     url(r'^$', 'administradorCentro_inicio',
         name='administradorCentro_inicio'),
 
+    url(r'^organizacion_institucional/$',
+        'administradorCentro_org_institucional',
+        name='administradorCentro_org_institucional'),
+
+    url(r'^organizacion_docente/$',
+        'administradorCentro_org_docente',
+        name='administradorCentro_org_docente'),
+
+    url(r'^alumnos/$',
+        'administradorCentro_alumnos',
+        name='administradorCentro_alumnos'),
+
+    url(r'^reuniones/$',
+        'administradorCentro_reuniones',
+        name='administradorCentro_reuniones'),
+
+    url(r'^plantillas/$',
+        'administradorCentro_plantillas',
+        name='administradorCentro_plantillas'),
+
     ### Includes a cada entidad ###
 
 )
--- Codigo/proyecto/asesorias/vistas/vistasAdministradorCentro.py
+++ Codigo/proyecto/asesorias/vistas/vistasAdministradorCentro.py
@@ -7,3 +7,32 @@ PATH = 'asesorias/UsuarioAdministradorCentro/'
 def administradorCentro_inicio(request):
     return render_to_response(PATH + 'administradorCentro_inicio.html',
         {'user': request.user})
+
+@login_required()
+def administradorCentro_org_institucional(request):
+    return render_to_response(
+        PATH + 'administradorCentro_org_institucional.html',
+        {'user': request.user})
+
+@login_required()
+def administradorCentro_org_docente(request):
+    return render_to_response(PATH +
+        'administradorCentro_org_docente.html',
+        {'user': request.user})
+
+@login_required()
+def administradorCentro_alumnos(request):
+    return render_to_response(PATH + 'administradorCentro_alumnos.html',
+        {'user': request.user})
+
+@login_required()
+def administradorCentro_reuniones(request):
+    return render_to_response(PATH +
+        'administradorCentro_reuniones.html',
+        {'user': request.user})
+
+@login_required()
+def administradorCentro_plantillas(request):
+    return render_to_response(PATH +
+        'administradorCentro_plantillas.html',
+        {'user': request.user})
