39476e2da493f67ca129b9f88ce5486e6159778a
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-10-20 00:18:51 +0000

    Corregido pequeño error.

--- Codigo/proyecto/asesorias/vistas/vistasAdministradorCentro.py
+++ Codigo/proyecto/asesorias/vistas/vistasAdministradorCentro.py
@@ -5,23 +5,6 @@ from django.shortcuts import render_to_response
 
 PATH = 'asesorias/UsuarioAdministradorCentro/'
 
-@login_required()
-def administradorCentro_inicio(request, centro):
-    return render_to_response(PATH + 'administradorCentro_inicio.html',
-        {'user': request.user, 'centro': centro})
-
-@login_required()
-def administradorCentro_org_institucional(request, centro):
-    return render_to_response(
-        PATH + 'administradorCentro_org_institucional.html',
-        {'user': request.user, 'centro': centro})
-
-@login_required()
-def administradorCentro_org_docente(request, centro):
-    return render_to_response(PATH +
-        'administradorCentro_org_docente.html',
-        {'user': request.user, 'centro': centro})
-
 # Funcion decoradora para comprobar el centro.
 def checkCentro(funcion):
     def inner(request, centro, *args, **kwargs):
@@ -30,3 +13,25 @@ def checkCentro(funcion):
         else:
             return HttpResponseRedirect(reverse('logout'))
     return inner
+
+@checkCentro
+@login_required
+def administradorCentro_inicio(request, centro):
+    return render_to_response(PATH + 'administradorCentro_inicio.html',
+        {'user': request.user, 'centro': centro})
+
+@checkCentro
+@login_required
+def administradorCentro_org_institucional(request, centro):
+    return render_to_response(
+        PATH + 'administradorCentro_org_institucional.html',
+        {'user': request.user, 'centro': centro})
+
+@checkCentro
+@login_required
+def administradorCentro_org_docente(request, centro):
+    return render_to_response(PATH +
+        'administradorCentro_org_docente.html',
+        {'user': request.user, 'centro': centro})
+
+
