dd13c4b20483272aaedd878c3d9e5fdf532c5b5f
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-12-12 21:26:47 +0000

    Reordenadas las urls de reuniones.

--- Codigo/proyecto/asesorias/urls/Asesor/reuniones.py
+++ Codigo/proyecto/asesorias/urls/Asesor/reuniones.py
@@ -7,24 +7,20 @@ VISTAS = 'asesorias.vistas.Asesor.'
 # ------------------------#
 
 urlpatterns = patterns(VISTAS + 'vistasReuniones',
+
+    # ------------- #
+    # Ver reuniones #
+    # ------------- #
+
     url(r'^selectAlumno/$',
         'selectAlumno', name='selectAlumno_Asesor'),
 
-    url(r'^addReunionGrupal/(?P<lista_alumnos>[\s\w\&]*)/$',
-        'addReunionGrupal', name='addReunionGrupal_Asesor'),
+    url(r'^listReunion/(?P<orden>[\s\w]*)/$',
+        'listReunion', name='listReunion_Asesor'),
 
-    url(r'^delReunionGrupal/(?P<fecha>[\s\w\-]+)/$',
-        'delReunionGrupal', name='delReunionGrupal_Asesor'),
-
-    url(r'^addAlumnoAReunionGrupal/(?P<lista_alumnos>[\s\w\&]*)/' +
-        '(?P<dni_pasaporte>[\s\w]+)/$',
-
-        'addAlumnoAReunionGrupal', name='addAlumnoAReunionGrupal'),
-
-    url(r'^delAlumnoAReunionGrupal/(?P<lista_alumnos>[\s\w\&]*)/' +
-        '(?P<dni_pasaporte>[\s\w]+)/$',
-
-        'delAlumnoAReunionGrupal', name='delAlumnoAReunionGrupal'),
+    # ---------------------- #
+    # Reuniones individuales #
+    # ---------------------- #
 
     url(r'^addReunion/(?P<dni_pasaporte>[\s\w]+)/$',
         'addReunion', name='addReunion_Asesor'),
@@ -34,16 +30,12 @@ urlpatterns = patterns(VISTAS + 'vistasReuniones',
 
         'delReunion', name='delReunion_Asesor'),
 
-    url(r'^listReunion/(?P<orden>[\s\w]*)/$',
-        'listReunion', name='listReunion_Asesor'),
-
     url(r'^showReunion/(?P<dni_pasaporte>[\s\w]+)/' +
         '(?P<id_reunion>\d+)/$',
 
         'showReunion', name='showReunion_Asesor'),
 
-    url(r'^showReunionGrupal/(?P<fecha>[\s\w\-]+)/$',
-        'showReunionGrupal', name='showReunionGrupal_Asesor'),
+    # Plantillas individuales #
 
     url(r'^addPlantillaAReunion/(?P<dni_pasaporte>[\s\w]+)/' +
         '(?P<id_reunion>\d+)/(?P<id_entrevista>\d*)/' +
@@ -63,6 +55,9 @@ urlpatterns = patterns(VISTAS + 'vistasReuniones',
         'addPlantillaAsesorAReunion',
         name='addPlantillaAsesorAReunion'),
 
+
+    # Preguntas individuales #
+
     url(r'^addPreguntaOficialAReunion/(?P<dni_pasaporte>[\s\w]+)/' +
         '(?P<id_reunion>\d+)/(?P<id_entrevista_oficial>[\s\w]+)/' +
         '(?P<id_pregunta_oficial>[\s\w]+)/$',
@@ -76,13 +71,6 @@ urlpatterns = patterns(VISTAS + 'vistasReuniones',
 
         'delPreguntaOficialAReunion',
         name='delPreguntaOficialAReunion'),
-
-    url(r'^delPreguntaOficialAReunionGrupal/(?P<fecha>[\s\w\-]+)/' +
-        '/(?P<id_entrevista_oficial>[\s\w]+)/' +
-        '(?P<id_pregunta_oficial>[\s\w]+)/$',
-
-        'delPreguntaOficialAReunionGrupal',
-        name='delPreguntaOficialAReunionGrupal'),
 
     url(r'^addPreguntaAsesorAReunion/(?P<dni_pasaporte>[\s\w]+)/' +
         '(?P<id_reunion>\d+)/(?P<id_entrevista_asesor>[\s\w]+)/' +
@@ -98,6 +86,31 @@ urlpatterns = patterns(VISTAS + 'vistasReuniones',
         'delPreguntaAsesorAReunion',
         name='delPreguntaAsesorAReunion'),
 
+    # ------------------ #
+    # Reuniones grupales #
+    # ------------------ #
+
+    url(r'^addReunionGrupal/(?P<lista_alumnos>[\s\w\&]*)/$',
+        'addReunionGrupal', name='addReunionGrupal_Asesor'),
+
+    url(r'^delReunionGrupal/(?P<fecha>[\s\w\-]+)/$',
+        'delReunionGrupal', name='delReunionGrupal_Asesor'),
+
+    url(r'^addAlumnoAReunionGrupal/(?P<lista_alumnos>[\s\w\&]*)/' +
+        '(?P<dni_pasaporte>[\s\w]+)/$',
+
+        'addAlumnoAReunionGrupal', name='addAlumnoAReunionGrupal'),
+
+    url(r'^delAlumnoAReunionGrupal/(?P<lista_alumnos>[\s\w\&]*)/' +
+        '(?P<dni_pasaporte>[\s\w]+)/$',
+
+        'delAlumnoAReunionGrupal', name='delAlumnoAReunionGrupal'),
+
+    url(r'^showReunionGrupal/(?P<fecha>[\s\w\-]+)/$',
+        'showReunionGrupal', name='showReunionGrupal_Asesor'),
+
+    # Plantillas grupales #
+
     url(r'^addPlantillaAReunionGrupal/(?P<fecha>[\s\w\-]+)/' +
         '(?P<id_entrevista>\d*)/(?P<tipo>[\s\w]*)/$',
 
@@ -110,10 +123,19 @@ urlpatterns = patterns(VISTAS + 'vistasReuniones',
         'addPlantillaOficialAReunionGrupal',
         name='addPlantillaOficialAReunionGrupal'),
 
-    url(r'^addPreguntaOficialAReunion/(?P<fecha>[\s\w\-]+)/' +
+    # Preguntas grupales #
+
+    url(r'^addPreguntaOficialAReunionGrupal/(?P<fecha>[\s\w\-]+)/' +
         '(?P<id_entrevista_oficial>[\s\w]+)/' +
         '(?P<id_pregunta_oficial>[\s\w]+)/$',
 
         'addPreguntaOficialAReunionGrupal',
         name='addPreguntaOficialAReunionGrupal'),
+
+    url(r'^delPreguntaOficialAReunionGrupal/(?P<fecha>[\s\w\-]+)/' +
+        '/(?P<id_entrevista_oficial>[\s\w]+)/' +
+        '(?P<id_pregunta_oficial>[\s\w]+)/$',
+
+        'delPreguntaOficialAReunionGrupal',
+        name='delPreguntaOficialAReunionGrupal'),
 )
