94948dbb897dfa32134b44252e546e3d19aebf3f
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-12-13 04:22:36 +0000

    Corregido pequeño error.

--- Codigo/proyecto/asesorias/urls/Asesor/reuniones.py
+++ Codigo/proyecto/asesorias/urls/Asesor/reuniones.py
@@ -70,7 +70,7 @@ urlpatterns = patterns(VISTAS + 'vistasReuniones',
         '(?P<id_pregunta_oficial>[\s\w]+)/$',
 
         'editRespuestaOficial',
-        name='editRespuestaOficial'),
+        name='editRespuestaOficial_Asesor'),
 
     url(r'^delPreguntaOficialAReunion/(?P<dni_pasaporte>[\s\w]+)/' +
         '(?P<id_reunion>\d+)/(?P<id_entrevista_oficial>[\s\w]+)/' +
@@ -91,7 +91,7 @@ urlpatterns = patterns(VISTAS + 'vistasReuniones',
         '(?P<id_pregunta_asesor>[\s\w]+)/$',
 
         'editRespuestaAsesor',
-        name='editRespuestaAsesor'),
+        name='editRespuestaAsesor_Asesor'),
 
     url(r'^delPreguntaAsesorAReunion/(?P<dni_pasaporte>[\s\w]+)/' +
         '(?P<id_reunion>\d+)/(?P<id_entrevista_asesor>[\s\w]+)/' +
