ff465674296663ed3a54e68720d6dbf25a44e10a
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-12-28 23:19:23 +0000

    Añadida confirmación para matricula.

--- Codigo/proyecto/asesorias/vistas/AdministradorCentro/vistasMatricula.py
+++ Codigo/proyecto/asesorias/vistas/AdministradorCentro/vistasMatricula.py
@@ -196,25 +196,33 @@ def delMatricula(request, centro, nombre_titulacion,
 
     # Si existe se elimina.
     if instancia_matricula:
-        instancia_matricula.borrar()
-        # Redirige a la pagina de listar matriculas.
-        return HttpResponseRedirect(
-            reverse('listMatricula_administradorCentro',
-                kwargs={'centro':
-                instancia_matricula.determinarNombreCentro(),
-                'nombre_titulacion':
-                instancia_matricula.determinarNombreTitulacion(),
-                'plan_estudios':
-                instancia_matricula.determinarPlanEstudios(),
-                'nombre_asignatura':
-                instancia_matricula.determinarNombreAsignatura(),
-                'curso_academico': curso_academico,
-                'orden': 'curso_academico'}))
+        # Se carga el formulario de confirmacion.
+        form = forms.RealizarConfirmacion()
+        # Se ha modificado el formulario original.
+        if request.method == 'POST':
+            form = forms.RealizarConfirmacion(request.POST)
+            confirmacion = request.POST['confirmacion']
+
+            if confirmacion == 'True':
+                instancia_matricula.borrar()
+            # Redirige a la pagina de listar matriculas.
+            return HttpResponseRedirect(
+                reverse('listMatricula_administradorCentro',
+                    kwargs={'centro':
+                    instancia_matricula.determinarNombreCentro(),
+                    'nombre_titulacion':
+                    instancia_matricula.determinarNombreTitulacion(),
+                    'plan_estudios':
+                    instancia_matricula.determinarPlanEstudios(),
+                    'nombre_asignatura':
+                    instancia_matricula.determinarNombreAsignatura(),
+                    'curso_academico': curso_academico,
+                    'orden': 'curso_academico'}))
     # La matricula no existe.
     else:
-        error = True
+        form = True
     return render_to_response(PATH + 'delMatricula.html',
-        {'user': request.user, 'error': error, 'centro': centro})
+        {'user': request.user, 'form': form, 'centro': centro})
 
 @checkCentro
 @login_required
