7f195bd1cb31b2800aee011c77d39a211b537aa8
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2010-12-28 19:35:29 +0000

    Añadida confirmación para PEA.

--- Codigo/proyecto/asesorias/vistas/AdministradorPrincipal/vistasPlantillaEntrevistaAsesor.py
+++ Codigo/proyecto/asesorias/vistas/AdministradorPrincipal/vistasPlantillaEntrevistaAsesor.py
@@ -241,20 +241,28 @@ def delPlantillaEntrevistaAsesor(request, dni_pasaporte,
         id_entrevista_asesor)
     # Si existe se elimina.
     if instancia_plantilla_entrevista_asesor:
-        instancia_plantilla_entrevista_asesor.borrar()
+        # Se carga el formulario de confirmacion.
+        form = forms.RealizarConfirmacion()
+        # Se ha modificado el formulario original.
+        if request.method == 'POST':
+            form = forms.RealizarConfirmacion(request.POST)
+            confirmacion = request.POST['confirmacion']
 
-        # Redirige a la pagina de listar plantillas de entrevista de
-        # asesor.
-        return HttpResponseRedirect(
-            reverse('listPlantillaEntrevistaAsesor',
-                kwargs={'dni_pasaporte': dni_pasaporte,
-                'curso_academico': curso_academico,
-                'orden': 'descripcion'}))
+            if confirmacion == 'True':
+                instancia_plantilla_entrevista_asesor.borrar()
+
+            # Redirige a la pagina de listar plantillas de entrevista de
+            # asesor.
+            return HttpResponseRedirect(
+                reverse('listPlantillaEntrevistaAsesor',
+                    kwargs={'dni_pasaporte': dni_pasaporte,
+                    'curso_academico': curso_academico,
+                    'orden': 'descripcion'}))
     # La plantilla no existe.
     else:
-        error = True
+        form = True
     return render_to_response(PATH +'delPlantillaEntrevistaAsesor.html',
-        {'user': request.user, 'error': error})
+        {'user': request.user, 'form': form})
 
 @checkAdministradorPrincipal
 @login_required
