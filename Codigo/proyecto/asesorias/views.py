97cc0fdc33237b1a2dbc410243bfe48f67d2b5cf
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2011-01-23 02:32:55 +0000

    Corregido pequeño error.

--- Codigo/proyecto/asesorias/views.py
+++ Codigo/proyecto/asesorias/views.py
@@ -1,11 +1,1 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
-from asesorias.vistas.AdministradorPrincipal import vistasAsesor
-from asesorias.vistas.AdministradorPrincipal import vistasAlumno
 
-@login_required()
-def alumno(request):
-    # Obtiene una instancia del alumno en el sistema.
-    alumno = vistasAlumno.obtenerAlumno( unicode(request.user) )
-
-    return render_to_response('asesorias/alumnos.html', {'alumno': alumno})
