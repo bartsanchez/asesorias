519d1c44ad2dcde5c40a76edf94c61cc10be82d5
elbarto <elbarto@cbb1a3d5-5783-bd3a-911a-ed392fdd4978>
2011-01-24 00:58:47 +0000

    Corregido pequeño error.

--- Codigo/proyecto/asesorias/vistas/vistasAsesor.py
+++ Codigo/proyecto/asesorias/vistas/vistasAsesor.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from asesorias import models
 from django.shortcuts import render_to_response
 from asesorias.vistas.AdministradorPrincipal import \
     vistasAsesorCursoAcademico
@@ -8,35 +9,52 @@ from asesorias import forms
 
 PATH = 'asesorias/UsuarioAsesor/'
 
+# Funcion decoradora para comprobar el asesor.
+def checkAsesor(funcion):
+    def inner(request, curso_academico, *args, **kwargs):
+        try:
+            models.Asesor.objects.get(
+                dni_pasaporte=unicode(request.user))
+        except:
+            return HttpResponseRedirect(reverse('logout'))
+        return funcion(request, curso_academico, *args, **kwargs)
+    return inner
+
+@checkAsesor
 @login_required()
 def asesor_inicio(request, curso_academico):
     return render_to_response(PATH + 'asesor_inicio.html',
         {'user': request.user, 'curso_academico': curso_academico})
 
+@checkAsesor
 @login_required()
 def asesor_informacion_personal(request, curso_academico):
     return render_to_response(
         PATH + 'asesor_informacion_personal.html',
         {'user': request.user, 'curso_academico': curso_academico})
 
+@checkAsesor
 @login_required()
 def asesor_alumnos(request, curso_academico):
     return render_to_response(
         PATH + 'asesor_alumnos.html',
         {'user': request.user, 'curso_academico': curso_academico})
 
+@checkAsesor
 @login_required()
 def asesor_plantillas(request, curso_academico):
     return render_to_response(
         PATH + 'asesor_plantillas.html',
         {'user': request.user, 'curso_academico': curso_academico})
 
+@checkAsesor
 @login_required()
 def asesor_reuniones(request, curso_academico):
     return render_to_response(
         PATH + 'asesor_reuniones.html',
         {'user': request.user, 'curso_academico': curso_academico})
 
+@checkAsesor
 @login_required()
 def setCursoAcademico(request, curso_academico):
     # Se ha modificado el formulario original.
